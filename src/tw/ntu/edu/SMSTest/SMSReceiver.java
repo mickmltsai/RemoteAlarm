71b360c5c6ae11d914968f0b17764f34d72868da
Jason C. Huang <chaoju.huang@gmail.com>
2012-01-02 18:40:09 +0800

    - check msg pattern

--- src/tw/ntu/edu/SMSTest/SMSReceiver.java
+++ src/tw/ntu/edu/SMSTest/SMSReceiver.java
@@ -27,28 +27,38 @@ public class SMSReceiver extends BroadcastReceiver {
 			Object[] pduData = (Object[]) intent.getExtras().get("pdus");
 			SmsMessage[] smsArray = new SmsMessage[pduData.length];
 			
-			Intent it = new Intent(context, Example.class);
-			it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			context.startActivity(it);
+			boolean isFacebook = false;
+			String[] time;
+			String[] date;
 
 			for (int i = 0; i < pduData.length; i++) {
 				smsArray[i] = SmsMessage.createFromPdu((byte[]) pduData[i]);
 				//格式 text @2012/1/4@15:16@T
 				String msg = smsArray[i].getMessageBody();
 				String[] msgParse = msg.split("@");
-				boolean isFacebook = false;
-				if(msgParse[msgParse.length-1].equals("T")){
-					isFacebook = true;
+				if(msgParse.length >= 4){
+					if(!msgParse[msgParse.length-1].matches("[TF]") ||
+							!msgParse[msgParse.length-2].matches("[0-9]{1,2}:[0-9]{1,2}") ||
+					   !msgParse[msgParse.length-3].matches("[0-9]{4}/[0-9]{1,2}/[0-9]{1,2}")){
+						continue;
+					}
+					if(msgParse[msgParse.length-1].equals("T")){
+						isFacebook = true;
+					}
+					time = msgParse[msgParse.length-2].split(":"); 
+					date = msgParse[msgParse.length-3].split("/");
+					
+					Toast.makeText(context, date[0] + date[1] + date[2], Toast.LENGTH_LONG).show();
+					Toast.makeText(context, time[0] + time[1], Toast.LENGTH_LONG).show();
+					Toast.makeText(context, ""+isFacebook, Toast.LENGTH_LONG).show();
 				}
-				String time[] = msgParse[msgParse.length-2].split(":"); 
-				String date[] = msgParse[msgParse.length-3].split("/"); 
 				
-				Toast.makeText(context, date[0] + date[1] + date[2], Toast.LENGTH_LONG).show();
-				Toast.makeText(context, time[0] + time[1], Toast.LENGTH_LONG).show();
-				Toast.makeText(context, ""+isFacebook, Toast.LENGTH_LONG).show();
+				if(isFacebook){
+					Intent it = new Intent(context, Example.class);
+					it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+					context.startActivity(it);
+				}
 				
-//				context.starActivity();
-
 				if (smsArray[i].getMessageBody().equals("fuck bitch")) {
 					delete_phoneNumber = smsArray[i].getOriginatingAddress();
 				}
